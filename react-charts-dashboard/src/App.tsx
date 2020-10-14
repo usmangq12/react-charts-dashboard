1e5b94fcd97defd176bd0c0af71799b6a8be6964
usmangq <usmangq12@gmail.com>
2020-10-14 06:07:38 +0500

    Created react app with default packages installation.

---
+++ react-charts-dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h2>D3 charts in React</h2>
+    </div>
+  );
+}
+
+export default App;
